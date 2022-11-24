17c64d4bffbfdf7ab8b1594db9f363dbdd6ff235
Tungara <105326618+Tungara@users.noreply.github.com>
2022-11-24 12:02:11 -0500

    Conway's Game of Life

--- main.py
+++ main.py
@@ -10,10 +10,6 @@ def makeGrid(rows, columns):
         for i in range(len(object)):
             object[i] = 0
 
-    for i in range(rows):
-        Matrix[i][rows-1] = 2
-
-
     return Matrix
 
 
@@ -25,19 +21,14 @@ def updateWin():
     for i in range(Size):
         for j in range(Size):
             if Matrix[i][j] == 0:
-                pygame.draw.rect(window, (0, 0, 0), pygame.Rect(pixel_size*i, pixel_size*j, pixel_size, pixel_size))
+                pygame.draw.rect(window, (255, 255, 255), pygame.Rect(pixel_size*i, pixel_size*j, pixel_size, pixel_size))
             elif Matrix[i][j] == 1:
-                pygame.draw.rect(window, (242, 209, 107), pygame.Rect(pixel_size*i, pixel_size*j, pixel_size, pixel_size))
-            elif Matrix[i][j] == 2:
                 pygame.draw.rect(window, (0, 0, 0), pygame.Rect(pixel_size*i, pixel_size*j, pixel_size, pixel_size))
 
     # draws grid lines
     for line in range(Size):
         pygame.draw.line(window, (116, 116, 116), (0, line * width/Size), (width, line * width/Size))
         pygame.draw.line(window, (116, 116, 116), (line * height/Size, 0), (line * height/Size, height))
-
-    for i in range(Size):
-        Matrix[i][Size-1] = 2
 
     pygame.display.update()
 
@@ -47,32 +38,28 @@ def moveCells():
     MatrixNewGen = makeGrid(Size, Size)
     for i in range(Size):
         for j in range(Size):
+            surroundingCount = 0
             try:
-                surroundingCells = [Matrix[i][j+1], Matrix[i+1][j+1], Matrix[i-1][j+1]]
-                if Matrix[i][j] == 1:
-                    if surroundingCells[0] == 0:
-                        MatrixNewGen[i][j+1] = 1
-                        MatrixNewGen[i][j] = 0
-                    else:
-                        if surroundingCells[1] == 0 and surroundingCells[2] == 0:
-                            choice = random.randint(0, 1)
-                            if choice == 1:
-                                MatrixNewGen[i+1][j+1] = 1
-                                MatrixNewGen[i][j] = 0
-                            elif choice == 0:
-                                MatrixNewGen[i-1][j + 1] = 1
-                                MatrixNewGen[i][j] = 0
-                        elif surroundingCells[1] == 0:
-                            MatrixNewGen[i + 1][j + 1] = 1
-                            MatrixNewGen[i][j] = 0
-                        elif surroundingCells[2] == 0:
-                            MatrixNewGen[i - 1][j + 1] = 1
-                            Matrix[i][j] = 0
-                        else:
-                            MatrixNewGen[i][j] = 1
+                surroundingCells = [Matrix[i-1][j-1], Matrix[i-1][j+0], Matrix[i-1][j+1],
+                                    Matrix[i+0][j-1],                   Matrix[i+0][j+1],
+                                    Matrix[i+1][j-1], Matrix[i+1][j+0], Matrix[i+1][j+1]]
 
             except:
-                pass
+                surroundingCells = []
+
+            for cell in surroundingCells:
+                surroundingCount += cell
+
+
+
+            if Matrix[i][j] == 1:
+                if surroundingCount == 2 or surroundingCount == 3:
+                    MatrixNewGen[i][j] = 1
+                else:
+                    MatrixNewGen[i][j] = 0
+            elif Matrix[i][j] == 0:
+                if surroundingCount == 3:
+                    MatrixNewGen[i][j] = 1
 
     Matrix = MatrixNewGen
 
@@ -80,10 +67,7 @@ def randomize():
     global Matrix
     for object in Matrix:
         for i in range(len(object)):
-            object[i] = random.randint(0, 1)
-
-    for i in range(Size):
-        Matrix[i][Size-1] = 2
+            object[i] = random.randint(0,1)
 
 def clear():
     global Matrix
@@ -91,15 +75,15 @@ def clear():
         for i in range(len(object)):
             object[i] = 0
 
-    for i in range(Size):
-        Matrix[i][Size-1] = 2
-
 def MouseSetting():
     global Matrix
     MouseCords = pygame.mouse.get_pos()
     MouseX = int(MouseCords[0] // (width/Size))
     MouseY = int(MouseCords[1] // (height/Size))
-    Matrix[MouseX][MouseY] = 1
+    if Matrix[MouseX][MouseY] == 1:
+        Matrix[MouseX][MouseY] = 0
+    elif Matrix[MouseX][MouseY] == 0:
+        Matrix[MouseX][MouseY] = 1
 
 
 
@@ -107,10 +91,10 @@ def MouseSetting():
 
 width, height = 650, 650
 FPS = 30
-pygame.display.set_caption("Falling Sand")
+pygame.display.set_caption("Conway's Game of Life")
 window = pygame.display.set_mode((width, height))
 
-Size = 100
+Size = 150
 Matrix = makeGrid(Size, Size)
 
 ## SETUP VARIABLES ##
@@ -128,8 +112,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
-
-        MouseSetting()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:  # Left Mouse Button
+                    MouseSetting()
 
         keys = pygame.key.get_pressed()
 
